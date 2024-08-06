3843b855830c5ae8e34d1ff0dba31b7a843fec93
Niranjan190607 <niranjan.rb1907@gmail.com>
2024-08-06 14:13:04 +0530

    Initial commit

---
+++ src/main/java/net/javathecode/spring_annotaations/SpringAnnotaationsApplication.java
@@ -0,0 +1,13 @@
+package net.javathecode.spring_annotaations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotaationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnnotaationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javathecode/spring_annotaations/SpringAnnotaationsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javathecode.spring_annotaations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnotaationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
